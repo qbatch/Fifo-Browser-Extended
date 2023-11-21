4365ca06317a9112057e63131edfe9680e3ac423
Abdullah Tariq <139118353+Abdullah-Tariq-qbatch@users.noreply.github.com>
2023-11-21 17:49:38 +0500

    fix script (#9)

--- src/preloads/view-preload.ts
+++ src/preloads/view-preload.ts
@@ -9,15 +9,128 @@ import { injectChromeWebstoreInstallButton } from './chrome-webstore';
 import { contextBridge } from 'electron';
 const tabId = ipcRenderer.sendSync('get-webcontents-id');
 
-const { LANGUAGE_CODE, LANGUAGES_CODE, PLUGINS_CODE, COOKIES_ENABLED_CODE, DNT_HEADER_CODE, TIME_ZONE_CODE } = process.env;
+const { LANGUAGE, PLUGINS, COOKIES_ENABLED, DNT_HEADER, TIME_ZONE_OFFSET, TIME_ZONE } = process.env;
+
+const timeZoneCode = `{
+  const OriginalDate = Date;
+  const updates = []; 
+  const prefs = new Proxy({
+    timezone: '${TIME_ZONE}',
+    offset: ${TIME_ZONE_OFFSET}
+  }, {
+    set(target, prop, value) {
+      target[prop] = value;
+      if (prop === 'offset') { updates.forEach(c => c());}
+      return true;
+    }
+  });
+  class SpoofDate extends Date {
+    #ad;
+    #sync() {
+      const offset = (prefs.offset + super.getTimezoneOffset());
+      this.#ad = new OriginalDate(this.getTime() + offset * 60 * 1000);
+    }
+    constructor(...args) {
+      super(...args);
+      updates.push(() => this.#sync());
+      this.#sync();
+    }
+    getTimezoneOffset() { return prefs.offset; }
+    toTimeString() {
+      if (isNaN(this)) { return super.toTimeString();}
+      const parts = super.toLocaleString.call(this, 'en', {
+        timeZone: prefs.timezone,
+        timeZoneName: 'longOffset'
+      }).split('GMT');
+      if (parts.length !== 2) { return super.toTimeString(); }
+      const a = 'GMT' + parts[1].replace(':', '');
+      const b = super.toLocaleString.call(this, 'en', {
+        timeZone: prefs.timezone,
+        timeZoneName: 'long'
+      }).split(/(AM |PM )/i).pop();
+      return super.toTimeString.apply(this.#ad).split(' GMT')[0] + ' ' + a + ' (' + b + ')';
+    }
+    toDateString() { return super.toDateString.apply(this.#ad); }
+    toString() { return this.toDateString() + ' ' + this.toTimeString(); }
+    toLocaleDateString(...args) {
+      args[1] = args[1] || {};
+      args[1].timeZone = args[1].timeZone || prefs.timezone;
+      return super.toLocaleDateString(...args);
+    }
+    toLocaleTimeString(...args) {
+      args[1] = args[1] || {};
+      args[1].timeZone = args[1].timeZone || prefs.timezone;
+      return super.toLocaleTimeString(...args);
+    }
+    toLocaleString(...args) {
+      args[1] = args[1] || {};
+      args[1].timeZone = args[1].timeZone || prefs.timezone;
+      return super.toLocaleString(...args);
+    }
+    #get(name, ...args) { return super[name].call(this.#ad, ...args); }
+    getTimezoneOffset(...args) { return prefs.offset; }
+    getDate(...args) { return this.#get('getDate', ...args); }
+    getDay(...args) { return this.#get('getDay', ...args); }
+    getHours(...args) { return this.#get('getHours', ...args); }
+    getMinutes(...args) { return this.#get('getMinutes', ...args); }
+    getMonth(...args) { return this.#get('getMonth', ...args); }
+    getYear(...args) { return this.#get('getYear', ...args); }
+    getFullYear(...args) { return this.#get('getFullYear', ...args); }
+    #set(type, name, args) {
+      if (type === 'ad') {
+        const n = this.#ad.getTime();
+        const r = this.#get(name, ...args);
+        return super.setTime(this.getTime() + r - n);
+      }
+      else {
+        const r = super[name](...args);
+        this.#sync();
+        return r;
+      }
+    }
+    setHours(...args) { return this.#set('ad', 'setHours', args); }
+    setMinutes(...args) { return this.#set('ad', 'setMinutes', args); }
+    setMonth(...args) { return this.#set('ad', 'setMonth', args); }
+    setDate(...args) { return this.#set('ad', 'setDate', args); }
+    setYear(...args) { return this.#set('ad', 'setYear', args); }
+    setFullYear(...args) { return this.#set('ad', 'setFullYear', args); }
+    setTime(...args) { return this.#set('md', 'setTime', args); }
+    setUTCDate(...args) { return this.#set('md', 'setUTCDate', args); }
+    setUTCFullYear(...args) { return this.#set('md', 'setUTCFullYear', args); }
+    setUTCHours(...args) { return this.#set('md', 'setUTCHours', args); }
+    setUTCMinutes(...args) { return this.#set('md', 'setUTCMinutes', args); }
+    setUTCMonth(...args) { return this.#set('md', 'setUTCMonth', args);}
+  }
+  self.Date = SpoofDate;
+  self.Date = new Proxy(Date, {
+    apply(target, self, args) {
+      return new SpoofDate(...args);
+    }
+  });
+  const DateTimeFormat = Intl.DateTimeFormat;
+  const script = document.currentScript;
+  class SpoofDateTimeFormat extends Intl.DateTimeFormat {
+    constructor(...args) {
+      if (!args[1]) { args[1] = {}; }
+      if (!args[1].timeZone) { args[1].timeZone = prefs.timezone; }
+      super(...args);
+    }
+  }
+  Intl.DateTimeFormat = SpoofDateTimeFormat;
+  Intl.DateTimeFormat = new Proxy(Intl.DateTimeFormat, {
+    apply(target, self, args) { return new Intl.DateTimeFormat(...args); }
+  });
+}`;
+
+const createMaskingString = (obj: any, property: any, val: any) => `Object.defineProperty(${obj}, "${property}", { value: ${val} })`;
 
 (async () => {
-  await webFrame.executeJavaScript(LANGUAGE_CODE);
-  await webFrame.executeJavaScript(LANGUAGES_CODE);
-  await webFrame.executeJavaScript(PLUGINS_CODE);
-  await webFrame.executeJavaScript(COOKIES_ENABLED_CODE);
-  await webFrame.executeJavaScript(DNT_HEADER_CODE);
-  await webFrame.executeJavaScript(TIME_ZONE_CODE)
+  await webFrame.executeJavaScript(createMaskingString('navigator', 'language', `'${LANGUAGE}'`));
+  await webFrame.executeJavaScript(createMaskingString('navigator', 'languages', `['${LANGUAGE}']`));
+  await webFrame.executeJavaScript(createMaskingString('navigator', 'plugins', PLUGINS));
+  await webFrame.executeJavaScript(createMaskingString('navigator', 'cookieEnabled', COOKIES_ENABLED));
+  await webFrame.executeJavaScript(createMaskingString('navigator', 'doNotTrack', DNT_HEADER));
+  await webFrame.executeJavaScript(timeZoneCode)
 })();
 
 export const windowId: number = ipcRenderer.sendSync('get-window-id');
